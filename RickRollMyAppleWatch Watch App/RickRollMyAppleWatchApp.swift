bec37bb188612a94fa8dabb47cf3d4bab89f59a9
Cesar Gonzalez <cesargon4011@gmail.com>
2023-02-05 17:40:44 -0800

    Starting Base Project (DO NOT TRY TO USE THIS YET)

---
+++ RickRollMyAppleWatch Watch App/RickRollMyAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickRollMyAppleWatchApp.swift
+//  RickRollMyAppleWatch Watch App
+//
+//  Created by Cesar F. Gonzalez on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RickRollMyAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
